4de29861a268159ef66e2cf845342f8c3de6f6f6
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-12-14 18:36:36 +0530

    image url change to array of strings and db error rectified

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config()
 
 const connect = ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.gk6g4.mongodb.net/test `)
+    return mongoose.connect(`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.gk6g4.mongodb.net/tanishq`)
 }
 
 module.exports = connect ;
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name : {type : String, required: true},
     price:{type : String, required: true} ,
     description : {type : String, required: true} ,
-    image_url : { type : String, required: true} ,
+    image_url : [{ type : String, required: true}],
     weight : { type : String, required: true},
 } , {
     versionKey : false ,
