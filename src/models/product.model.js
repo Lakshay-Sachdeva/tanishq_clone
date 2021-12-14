bf55c52de407f1318102dd4ed37fb1ebcc826a30
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-12-14 17:32:25 +0530

    product model and schema

---
+++ src/models/product.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name : {type : String, required: true},
+    price:{type : String, required: true} ,
+    description : {type : String, required: true} ,
+    image_url : { type : String, required: true} ,
+    weight : { type : String, required: true},
+} , {
+    versionKey : false ,
+    timestamps : true
+}) ;
+
+const Product = mongoose.model("product" , productSchema) ;
+module.exports = Product ;
