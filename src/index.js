99a017e21acbeb7498a96d4498ad65549b75da99
AbhijitAher <seabhi.aher@gmail.com>
2021-12-15 11:51:14 +0530

    EJS setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const productController = require("./controllers/product.controller") ;
 const app = express();
 app.use(express.json()) ;
 
+app.set("view engine", "ejs");
+
 app.use("/products" , productController) ;
 
 module.exports = app ;
