46e8cb10efa65d1f6a5ef39ac64fed2883bd89b5
Gajanan Wattamwar <wattamwargaj@gmail.com>
2021-12-15 23:06:06 +0530

    home_page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+<<<<<<< Updated upstream
+=======
+const navbar = require("./views/scripts/navbar");
+
+const footer = require("./views/scripts/footer");
+
+>>>>>>> Stashed changes
 const productController = require("./controllers/product.controller") ;
 
 const app = express();
@@ -7,4 +14,24 @@ app.use(express.json()) ;
 
 app.use("/products" , productController) ;
 
+<<<<<<< Updated upstream
+=======
+app.get("/Tanishq", async (req, res) => {
+
+    res.render("../index", {
+        navbar: navbar,
+        footer: footer,
+    });
+})
+
+app.get("/Tanishq/products", async(req, res) => {
+    const product_data = await Product.find().lean().exec();
+    res.render("products_page", {
+        product_data: product_data,
+    });
+})
+
+
+
+>>>>>>> Stashed changes
 module.exports = app ;
