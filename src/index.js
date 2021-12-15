0388824037265f3e96de11a2a27ff92574d13e11
AbhijitAher <seabhi.aher@gmail.com>
2021-12-15 14:07:30 +0530

    added 2 routes in index.js - setup tailwind in index.ejs - added views folder with some files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 
+const navbar = require("../views/scripts/navbar");
+
 const productController = require("./controllers/product.controller") ;
 
+const Product = require('./models/product.model') ;
+
 const app = express();
+
 app.use(express.json()) ;
 
 app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.use("/products" , productController) ;
+
+app.get("/Tanishq", async (req, res) => {
+
+    res.render("../index", {
+        navbar: navbar,
+    });
+})
+
+app.get("/Tanishq/products", async(req, res) => {
+    const product_data = await Product.find().lean().exec();
+    res.render("products_page", {
+        product_data: product_data,
+    });
+})
+
+
 
 module.exports = app ;
