a29eb3084e89604a696f447ba260dfca3ecde872
Lakshay-Sachdeva <87421868+Lakshay-Sachdeva@users.noreply.github.com>
2021-12-27 21:08:54 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./index.js");
 
 const connect = require("./configs/db");
 
-app.listen(2424, async () => {
+app.listen(80, async () => {
 	await connect();
-	console.log("Listening on port number 2424");
+	console.log("Listening on port number 80");
 });
